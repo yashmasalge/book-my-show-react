11d69383ab55f209eb4e9468b573c537614260a4
yashmasalge <myash2101@gmail.com>
2022-01-13 12:02:30 +0530

    added delete movie

--- src/components/SingleMovieFetch.js
+++ src/components/SingleMovieFetch.js
@@ -3,49 +3,73 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import axios from "axios";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import Button from "react-bootstrap/Button";
+import { useHistory } from "react-router-dom";
 
 function SingleMovieFetch() {
-    const [data, setData] = useState([]);
-    let {_id} = useParams();
+  const [data, setData] = useState([]);
+  let { _id } = useParams();
 
-    useEffect(async () => {
-        try {
-            const response = await axios.get(`https://bms-backend1.herokuapp.com/movies/${_id}`);
-            console.log(response);
-            setData([response.data]);
-        } catch (error) {
-            console.error(error);
-        }
-    }, []);
+  useEffect(async () => {
+    try {
+      const response = await axios.get(
+        `https://bms-backend1.herokuapp.com/movies/${_id}`
+      );
+      console.log(response);
+      setData([response.data]);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
-    return (
-        <div>
-           <div>
-           <Container fluid style={{padding:"5%", background:"pink"}}>
-                <Row style={{textAlign:"center"}}>
-                {data.map((mov) =>{
-                        return(
-                            <Col id={mov._id} xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
-                        <Card>
-                            <Card.Img variant="top" src={mov.imageurl} />
-                            <Card.Body>
-                            <Card.Title>{mov.title}</Card.Title>
-                            <Card.Text>
-                               {mov.actor}
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                        )
-                }  
-                )}
-                </Row>
-            </Container>
-        </div>
-        </div>
-    );
+  const history = useHistory();
+
+  const erase = () => {
+    try {
+      const response = axios.delete(
+        `https://bookmyshow-react.vercel.app/movies-delete/${_id}`
+      );
+      setData([response.data]);
+      history.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <Container fluid style={{ padding: "5%", background: "pink" }}>
+          <Row style={{ textAlign: "center" }}>
+            {data.map((mov) => {
+              return (
+                <Col
+                  id={mov._id}
+                  xs={6}
+                  md={4}
+                  lg={3}
+                  style={{ marginBottom: "2%" }}
+                >
+                  <Card>
+                    <Card.Img variant="top" src={mov.imageurl} />
+                    <Card.Body>
+                      <Card.Title>{mov.title}</Card.Title>
+                      <Card.Text>{mov.actor}</Card.Text>
+                      <Button onClick={erase} variant="danger">
+                        Delete
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
 
 export default SingleMovieFetch;
